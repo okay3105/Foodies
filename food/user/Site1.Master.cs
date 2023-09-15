30a5c1c6af1e41e5a23a9f01d41e57b3247f46e0
deepp <deepp@LNVD>
2023-09-15 16:16:49 +0530

    Add project files.

---
+++ food/user/Site1.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace food.user
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Defualt.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                //load the control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl1.ascx");
+
+                //add the control to the panel
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
---
+++ food/user/SliderUserControl1.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace food.user
+{
+    public partial class SliderUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
